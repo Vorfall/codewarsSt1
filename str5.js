3fbe5976674e08671d08e858e87f9b36c759521b
Vorfall <alekseevegorc@gmail.com>
2021-03-30 13:54:35 +0300

    add str5

---
+++ str5.js
@@ -0,0 +1,35 @@
+/*word = "di323123123n"
+length = word.length
+newstr=""
+count=0
+for(i=0;i<length;i++)
+{
+    console.log(word.length)
+    console.log(i)
+    count=countLet(word,word[i])
+
+    if(count>2)
+    {
+        newstr+=")"
+    }
+    else{
+        newstr+="(" 
+    }
+}
+console.log(newstr)
+
+function countLet(word,letter)
+{
+count=0
+for(i=0;i<word.length;i++)
+{
+    if(word[i]===letter)
+    {
+        count++
+    }
+    
+}
+console.log(count)
+    return count
+}*/
+
