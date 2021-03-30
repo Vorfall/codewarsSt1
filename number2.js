62359b4356e19652522fc553d0fc93b78316068f
Vorfall <alekseevegorc@gmail.com>
2021-03-30 13:54:09 +0300

    add number 2

---
+++ number2.js
@@ -0,0 +1,11 @@
+value="afbh124h1hfsdhgfh3"
+str={}
+for(i=0;i<value.length;i++)
+  {
+    if(value[i]==="1" ||value[i]==="2" ||value[i]==="3" ||value[i]==="4" ||value[i]==="5" ||value[i]==="6" ||value[i]==="7" ||value[i]==="8" ||value[i]==="9" ||value[i]==="0" )
+{
+       str+="value[i]"
+       
+       }
+  }
+cpnsole.log(str)
